65cf1c682f1943de1584d276eb9b253bcbac543b
clebson.araujo <clebson.araujo@l5.com.br>
2021-10-02 18:56:52 -0300

    corrigido problema de vizualização de data, alinhamento centralizado da coluna do sexo do bebê, efeito hover nos botões de opções e alinhamento do menu ao centro.

--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const conn = require('./../db')
 const router = express.Router()
+const moment = require('moment')
 
 router.get('/', function(req, res){
     let query = `SELECT * FROM clientes limit 5;`
@@ -8,7 +9,8 @@ router.get('/', function(req, res){
         if(err) console.log(err)
         res.render('index', {
             title: 'NewBorn Controle de Clientes',
-            data: result
+            data: result,
+            moment: moment
         })
     })
 })
